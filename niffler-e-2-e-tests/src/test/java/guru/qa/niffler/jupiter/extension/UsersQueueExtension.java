34b9bd1f4a8091b52790974cc933388d2c8b657d
aippolitov <franktanwork@googlemail.com>
2025-04-01 22:22:53 +0300

    чистка от старого кода

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
@@ -45,111 +45,84 @@ public class UsersQueueExtension implements
 
   @Override
   public void beforeTestExecution(ExtensionContext context) {
-    // Создаем HashMap для хранения всех пользователей, связанных с этим тестом.
-    Map<UserType, StaticUser> userMap = context.getStore(NAMESPACE)
+    // Создаем или получаем существующую HashMap для хранения пользователей
+    Map<UserType, StaticUser> userMap = (Map<UserType, StaticUser>) context.getStore(NAMESPACE)
             .getOrComputeIfAbsent(
                     context.getUniqueId(),
-                    key -> new HashMap<UserType, StaticUser>(),
-                    Map.class
+                    key -> new HashMap<>()
             );
-
-    // Обрабатываем все параметры тестового метода, аннотированные @UserType
+    // Обрабатываем все параметры с аннотацией @UserType
     Arrays.stream(context.getRequiredTestMethod().getParameters())
             .filter(p -> AnnotationSupport.isAnnotated(p, UserType.class))
             .forEach(parameter -> {
               UserType ut = parameter.getAnnotation(UserType.class);
               Optional<StaticUser> user = Optional.empty();
               StopWatch sw = StopWatch.createStarted();
+
+              // Ждем до 30 секунд, пытаясь получить пользователя из очереди
               while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
                 user = ut.empty()
                         ? Optional.ofNullable(EMPTY_USERS.poll())
                         : Optional.ofNullable(NOT_EMPTY_USERS.poll());
               }
+
               Allure.getLifecycle().updateTestCase(testCase ->
                       testCase.setStart(new Date().getTime())
               );
+
               if (user.isEmpty()) {
-                throw new IllegalStateException("Невозможно получить пользователя после 30 секунд.");
+                throw new IllegalStateException("Не удалось получить пользователя после 30 секунд ожидания.");
               }
+
+              // Сохраняем пользователя в HashMap с ключом UserType
               userMap.put(ut, user.get());
             });
-    /*
-    Arrays.stream(context.getRequiredTestMethod().getParameters())
-        .filter(p -> AnnotationSupport.isAnnotated(p, UserType.class))
-        .findFirst()
-        .map(p -> p.getAnnotation(UserType.class))
-        .ifPresent(ut -> {
-          Optional<StaticUser> user = Optional.empty();
-          StopWatch sw = StopWatch.createStarted();
-          while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
-            user = ut.empty()
-                ? Optional.ofNullable(EMPTY_USERS.poll())
-                : Optional.ofNullable(NOT_EMPTY_USERS.poll());
-          }
-          Allure.getLifecycle().updateTestCase(testCase ->
-              testCase.setStart(new Date().getTime())
-          );
-          user.ifPresentOrElse(
-              u ->
-                  context.getStore(NAMESPACE).put(
-                      context.getUniqueId(),
-                      u
-                  ),
-              () -> {
-                throw new IllegalStateException("Can`t obtain user after 30s.");
-              }
-          );
-        });
-     */
   }
 
   @Override
   public void afterTestExecution(ExtensionContext context) {
-    // Получаем HashMap из контекста.
-    Map<UserType, StaticUser> map = context.getStore(NAMESPACE)
-            .get(context.getUniqueId(), Map.class);
+    // Получаем HashMap с пользователями
+    Map<UserType, StaticUser> map = context.getStore(NAMESPACE).get(
+            context.getUniqueId(),
+            Map.class
+    );
+
     if (map != null) {
+      // Возвращаем всех пользователей в соответствующие очереди
       for (Map.Entry<UserType, StaticUser> entry : map.entrySet()) {
-        if (entry.getValue().empty()) {
-          EMPTY_USERS.add(entry.getValue());
+        StaticUser user = entry.getValue();
+        if (user.empty()) {
+          EMPTY_USERS.add(user);
         } else {
-          NOT_EMPTY_USERS.add(entry.getValue());
+          NOT_EMPTY_USERS.add(user);
         }
       }
-      // Очистка хранилища для данного теста.
+
+      // Очищаем хранилище после использования
       context.getStore(NAMESPACE).remove(context.getUniqueId());
     }
-    /*
-    StaticUser user = context.getStore(NAMESPACE).get(
-        context.getUniqueId(),
-        StaticUser.class
-    );
-    if (user.empty()) {
-      EMPTY_USERS.add(user);
-    } else {
-      NOT_EMPTY_USERS.add(user);
-    }
-     */
   }
 
   @Override
   public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
     return parameterContext.getParameter().getType().isAssignableFrom(StaticUser.class)
-        && AnnotationSupport.isAnnotated(parameterContext.getParameter(), UserType.class);
+            && AnnotationSupport.isAnnotated(parameterContext.getParameter(), UserType.class);
   }
 
   @Override
   public StaticUser resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
     UserType ut = parameterContext.getParameter().getAnnotation(UserType.class);
-    Map<UserType, StaticUser> map = extensionContext.getStore(NAMESPACE)
-            .get(extensionContext.getUniqueId(), Map.class);
+    Map<UserType, StaticUser> map = extensionContext.getStore(NAMESPACE).get(
+            extensionContext.getUniqueId(),
+            Map.class
+    );
+
+    // Получаем пользователя из HashMap по аннотации UserType
     StaticUser user = map.get(ut);
     if (user == null) {
-      throw new ParameterResolutionException("Нет соответствующего пользователя для параметра с аннотацией " + ut);
+      throw new ParameterResolutionException("Пользователь не найден для параметра с аннотацией " + ut);
     }
+
     return user;
-    /*
-    return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), StaticUser.class);
-     */
   }
 }
