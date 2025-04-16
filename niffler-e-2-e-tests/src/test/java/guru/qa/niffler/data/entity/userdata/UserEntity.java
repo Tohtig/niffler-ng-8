2659188a71a9c68b550ed3cb982dfa21cb324431
aippolitov <franktanwork@googlemail.com>
2025-04-16 14:37:57 +0300

    п.2-3 Реализовать UserdataUserDAOJdbc с методами create, findById, findByUsername и delete. Класс UserEntity нужно взять из сервиса niffler-userdata, убрав из него все, что касается дружбы (Friendship). DAO должен работать с БД niffler-userdata

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/entity/userdata/UserEntity.java
@@ -0,0 +1,36 @@
+package guru.qa.niffler.data.entity.userdata;
+
+import guru.qa.niffler.model.CurrencyValues;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class UserEntity implements Serializable {
+  private UUID id;
+  private String username;
+  private CurrencyValues currency;
+  private String firstname;
+  private String surname;
+  private String fullname;
+  private byte[] photo;
+  private byte[] photoSmall;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    UserEntity that = (UserEntity) o;
+    return getId() != null && Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return id != null ? id.hashCode() : 0;
+  }
+}
